d583b7e5653e4c299b557f3c3c3a221f2805a643
Canberk Aydemir <canberk.aydemir@yahoo.com>
2023-06-03 16:35:31 +0300

    Improved preprocessing

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
-st.write("Yeah Science")
+
+
+st.title("Banking Classification")
+
+st.write("Fill in the form with customer information to predict if they will subscribe to term deposit.")
+
+st.form(key = "banking")
+
